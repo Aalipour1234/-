51104655b5a7fb8320dce991dd18e3d49d973c53
علی علی پور <alialipourir123@gmail.com>
2025-01-19 05:48:29 +0330

    Add files via upload

---
+++ Program6.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i <= 40; i += 2) // حلقه برای اعداد زوج
+        {
+            if (i == 8) // شرط برای چاپ نکردن عدد 8
+                continue;
+
+            if (i >= 30) // شرط برای چاپ نکردن اعداد زوج 30 به بعد
+                break;
+
+            Console.WriteLine(i); // چاپ عدد
+        }
+    }
+}
